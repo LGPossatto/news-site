912b0959f81bb7ca9cb7f41fc495b4df238543fc
Luiz Gustavo <lgpossatto@gmail.com>
2021-03-07 11:36:54 -0300

    first commit

---
+++ src/conponents/navbar/Navbar.component.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.style.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="topbar container">
+        <h1 className="fs-big fc-primary">Salão de Notícias</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
